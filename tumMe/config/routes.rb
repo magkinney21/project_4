5ae567920c0b4796c87c3aac118761ba5122b528
Magkinney21 <magkinney21@gmail.com>
2017-05-10 13:03:03 -0400

    favorites controller and model and link

--- tumMe/config/routes.rb
+++ tumMe/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do
     resources :comments
-    resources :favorite_posts, only: [:create, :destroy]
+    resources :favorite_posts, only: [:index, :create, :destroy]
   end
   root to: "posts#index"
 end
