21742ec2c12b9b20983dbb733cb22926a7884163
Jcireland14 <Jcireland14@gmail.com>
2017-05-11 13:57:57 -0400

    seeded data but only user login recognizes seeded data

--- tumMe/db/schema.rb
+++ tumMe/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510202201) do
+ActiveRecord::Schema.define(version: 20170511175356) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170510202201) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "img_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
